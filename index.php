77f3341ce02e5b44c94dcd6bb454d3b987081aa4
Jason Ferrer <jferrer@csumb.edu>
2017-11-12 03:57:06 +0000

    Accordion for Info and ASC and DESC radios work now

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
     <body>
         <div class="container">
             
@@ -11,8 +14,8 @@
                         <option value="Director">Director</option>
                     </select>
                     
-                    <input type="radio" name="order" value="ascending">Asc
-                    <input type="radio" name="order" value="descending">Desc
+                    <input type="radio" name="order" value="ASC">Asc
+                    <input type="radio" name="order" value="DESC">Desc
                     <input type="submit" value="Search" name="submit">
                 </form>
                 
@@ -34,20 +37,80 @@
                         {
                             $sql .= " ORDER BY " . $_GET['filter'];
                         }
+                        else {
+                            $sql .= " ORDER BY Name";
+                        }
+                        
+                        if(isset($_GET['order']))
+                        {
+                            $sql .= " " . $_GET['order'];
+                        }
                     }
+                    else{
+                        $sql .= " ORDER BY Name";
+                    }
+                    
+                    
                     
                     //echo $sql;
                     $stmt = $conn->prepare($sql);
                     $stmt->execute();
-                    $record = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     
-                    foreach($record as $rec)
-                    {
-                        echo $rec['Name']."<br>";
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>Title</th>";
+                    echo "<th>Genre</th>";
+                    echo "<th>Director</th>";
+                    echo "<th>Price</th>";
+                    echo "</tr>";
+                    
+                    foreach($records as $record) {
+                        echo "<tr>";
+                        echo "<td><button class='accordion'>". $record["Name"] ."</button>";
+                        echo "<div class='panel'>";
+                        
+                        echo "<p>Summary:</p>";
+                        echo "<p>A summary column needs to be added in all of the</p>";
+                        echo "<p>tables for the product and needs to be called in the commented</p>";
+                        echo "<p>code below. Once done, uncomment code and delete this</p>";
+                        echo "<p>block of paragraph.</p>";
+                        
+                        // echo "<p>";
+                        // echo $record["Name of Column"];
+                        // echo "</p>";
+                        
+                        echo "</div>";
+                        echo "</td>";
+                        echo "<td>". $record["Genre"] ."</td>";
+                        echo "<td>". $record["Director"] ."</td>";
+                        echo "<td>". $record["Price"] ."</td>";
+                        echo "</tr>";
                     }
+                    echo "</table>";
+                    
+                    // foreach($record as $rec)
+                    // {
+                    //     echo $rec['Name']."<br>";
+                    // }
                 ?>
             </div>
         </div>
-
+    <script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function(){
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
+    }
+    </script>
     </body>
 </html>
