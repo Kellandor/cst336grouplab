6a7a6d6854d71bbd906bf2d95755534b3c4420ca
Jason Ferrer <jferrer@csumb.edu>
2017-11-14 23:46:55 +0000

    Fixed it ...I think

--- music.php
+++ music.php
@@ -2,7 +2,6 @@
     <head>
         <link rel="stylesheet" type="text/css" href="css/styles.css">
     </head>
-<<<<<<< HEAD
     <body>
         <div class="container">
             
@@ -13,18 +12,6 @@
                         <option value="Name">Title</option>
                         <option value="Genre">Genre</option>
                         <option value="Artist">Artist</option>
-=======
-    <body class="main">
-        <div class="container">
-            <h1>Not Blockbuster</h1>
-            <div class="formDiv">
-                <form >
-                    <select id="dropdown" name="filter">
-                        <option value="">Filter by...</option>
-                        <option value="Name">Title</option>
-                        <option value="Genre">Genre</option>
-                        <option value="Director">Director</option>
->>>>>>> master
                     </select>
                     
                     <input type="radio" name="order" value="ASC">Asc
@@ -35,13 +22,10 @@
                 <form action="cart.php">
                     <input type="submit" name="cart" value="Shopping Cart">
                 </form>
-<<<<<<< HEAD
-=======
                 
                 <form action="home.php">
                     <input type="submit" value="Back to home">
                 </form>
->>>>>>> master
             </div>
             
             <div class="textDiv">
@@ -50,11 +34,7 @@
                     
                     $conn = getDatabaseConnection();
                     
-<<<<<<< HEAD
                     $sql = "SELECT * FROM music WHERE 1";
-=======
-                    $sql = "SELECT * FROM movies WHERE 1";
->>>>>>> master
                     
                     if(isset($_GET['submit'])){
                         if(!empty($_GET['filter']))
@@ -85,11 +65,7 @@
                     echo "<tr>";
                     echo "<th>Title</th>";
                     echo "<th>Genre</th>";
-<<<<<<< HEAD
                     echo "<th>Artist</th>";
-=======
-                    echo "<th>Director</th>";
->>>>>>> master
                     echo "<th>Price</th>";
                     echo "</tr>";
                     
@@ -98,42 +74,23 @@
                         echo "<td><button class='accordion'>". $record["Name"] ."</button>";
                         echo "<div class='panel'>";
                         
-<<<<<<< HEAD
                         echo "<p>";
                         echo "Album Name: ". $record["Album"];
                         echo "</p>";
                         echo "<p>";
                         echo "Length: " . $record["LengthMin"] . " minutes";
-=======
-                        // echo "<p>Summary:</p>";
-                        // echo "<p>A summary column needs to be added in all of the</p>";
-                        // echo "<p>tables for the product and needs to be called in the commented</p>";
-                        // echo "<p>code below. Once done, uncomment code and delete this</p>";
-                        // echo "<p>block of paragraph.</p>";
-                        
-                        echo "<p>";
-                        echo $record["Summary"];
->>>>>>> master
                         echo "</p>";
                         
                         echo "</div>";
                         echo "</td>";
                         echo "<td>". $record["Genre"] ."</td>";
-<<<<<<< HEAD
                         echo "<td>". $record["Artist"] ."</td>";
-=======
-                        echo "<td>". $record["Director"] ."</td>";
->>>>>>> master
                         echo "<td>". $record["Price"] ."</td>";
                         echo "</tr>";
                     }
                     echo "</table>";
                     
-<<<<<<< HEAD
                     // foreach($records as $rec)
-=======
-                    // foreach($record as $rec)
->>>>>>> master
                     // {
                     //     echo $rec['Name']."<br>";
                     // }
--- tv.php
+++ tv.php
@@ -2,7 +2,6 @@
     <head>
         <link rel="stylesheet" type="text/css" href="css/styles.css">
     </head>
-<<<<<<< HEAD
     <body>
         <div class="container">
             
@@ -13,18 +12,6 @@
                         <option value="Name">Title</option>
                         <option value="Genre">Genre</option>
                         <option value="Program Creator">Creator</option>
-=======
-    <body class="main">
-        <div class="container">
-            <h1>Not Blockbuster</h1>
-            <div class="formDiv">
-                <form >
-                    <select id="dropdown" name="filter">
-                        <option value="">Filter by...</option>
-                        <option value="Name">Title</option>
-                        <option value="Genre">Genre</option>
-                        <option value="Director">Director</option>
->>>>>>> master
                     </select>
                     
                     <input type="radio" name="order" value="ASC">Asc
@@ -35,13 +22,10 @@
                 <form action="cart.php">
                     <input type="submit" name="cart" value="Shopping Cart">
                 </form>
-<<<<<<< HEAD
-=======
                 
                 <form action="home.php">
                     <input type="submit" value="Back to home">
                 </form>
->>>>>>> master
             </div>
             
             <div class="textDiv">
@@ -50,11 +34,7 @@
                     
                     $conn = getDatabaseConnection();
                     
-<<<<<<< HEAD
                     $sql = "SELECT * FROM tvshows WHERE 1";
-=======
-                    $sql = "SELECT * FROM movies WHERE 1";
->>>>>>> master
                     
                     if(isset($_GET['submit'])){
                         if(!empty($_GET['filter']))
@@ -76,11 +56,7 @@
                     
                     
                     
-<<<<<<< HEAD
-                    echo $sql;
-=======
                     //echo $sql;
->>>>>>> master
                     $stmt = $conn->prepare($sql);
                     $stmt->execute();
                     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -89,11 +65,7 @@
                     echo "<tr>";
                     echo "<th>Title</th>";
                     echo "<th>Genre</th>";
-<<<<<<< HEAD
                     echo "<th>Program Creator</th>";
-=======
-                    echo "<th>Director</th>";
->>>>>>> master
                     echo "<th>Price</th>";
                     echo "</tr>";
                     
@@ -102,16 +74,7 @@
                         echo "<td><button class='accordion'>". $record["Name"] ."</button>";
                         echo "<div class='panel'>";
                         
-<<<<<<< HEAD
                         echo "<h4>Summary:</h4>";
-=======
-                        // echo "<p>Summary:</p>";
-                        // echo "<p>A summary column needs to be added in all of the</p>";
-                        // echo "<p>tables for the product and needs to be called in the commented</p>";
-                        // echo "<p>code below. Once done, uncomment code and delete this</p>";
-                        // echo "<p>block of paragraph.</p>";
-                        
->>>>>>> master
                         echo "<p>";
                         echo $record["Summary"];
                         echo "</p>";
@@ -119,21 +82,13 @@
                         echo "</div>";
                         echo "</td>";
                         echo "<td>". $record["Genre"] ."</td>";
-<<<<<<< HEAD
                         echo "<td>". $record["Program Creator"] ."</td>";
-=======
-                        echo "<td>". $record["Director"] ."</td>";
->>>>>>> master
                         echo "<td>". $record["Price"] ."</td>";
                         echo "</tr>";
                     }
                     echo "</table>";
                     
-<<<<<<< HEAD
                     // foreach($records as $rec)
-=======
-                    // foreach($record as $rec)
->>>>>>> master
                     // {
                     //     echo $rec['Name']."<br>";
                     // }
